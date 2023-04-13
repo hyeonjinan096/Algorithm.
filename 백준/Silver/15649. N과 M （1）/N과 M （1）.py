ba4aa8083f882a7fadc2b3d27bfc5a1a5afa8a86
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-04-13 20:45:36 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,26 +1,16 @@
 import sys
-
-n,m = map(int,sys.stdin.readline().split())
-numbers = [0 for i in range(m)]
+n, m = map(int,sys.stdin.readline().split())
+num = [0 for i in range(m)]
 
 def dfs(index):
-    if index == m:
-        #list 출력함수
-        for i in numbers:
-            print(i, end = " ")
-        print()
-        return
-
+    if index == m :
+        #for i in num:
+        #    print(i, end =" ")
+        print(" ".join(map(str,num)))
+        return 
     for i in range(1, n+1):
-        if i not in numbers:
-            numbers[index] = i
-            dfs(index + 1)
-            numbers[index] = 0
-
+        if i not in num:
+            num[index] = i
+            dfs(index+1)
+            num[index] = 0
 dfs(0)
-
-        
-
-
-
-
