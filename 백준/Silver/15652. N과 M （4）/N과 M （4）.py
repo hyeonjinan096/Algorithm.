f820a78f543bd8afed02ce4a17ccfbcddd3b850b
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-04-13 21:18:19 +0900

    [Silver III] Title: N과 M (4), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+import sys
+n, m = map(int,sys.stdin.readline().split())
+num = [0 for i in range(m)]
+
+def dfs(index):
+    if(index ==m):
+        print(" ".join(map(str,num)))
+        return
+    for i in range(1, n+1):
+        if(max(num)<=i):
+            num[index] = i
+            dfs(index+1)
+            num[index] = 0
+dfs(0)
