e14a24edba4d64541ae6d64afcf626aa5ffedbfa
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-04-15 01:32:06 +0900

    [Silver II] Title: 나무 자르기, Time: 4904 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,19 +5,15 @@ arr = list(map(int,sys.stdin.readline().split()))
 
 arr.sort()
 
-y = 0
 sum = 0
-s = 0
-e = max(arr)
-f=0
+s,e =0,max(arr)
+
 while(s<=e):
     M = (s+e)//2
-    #print("[%d]"%(M))
     sum = 0
     for i in arr:
         if(i>M):
             sum+=(i-M)
-    #print("<%d>"%(sum))
     if(sum>=m):
         s=M+1
     else:
