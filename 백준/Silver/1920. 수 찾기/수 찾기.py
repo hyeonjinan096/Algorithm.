f3788f7077820120a0924f6b2e7189563faf5090
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-04-14 20:43:15 +0900

    [Silver IV] Title: 수 찾기, Time: 192 ms, Memory: 51724 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -5,22 +5,13 @@ a = list(map(int,sys.stdin.readline().split()))
 m = int(input())
 b = list(map(int,sys.stdin.readline().split()))
 
-a.sort()
-
+map = {}
+for i in a:
+    map[i] = 0
 
 for i in b:
-    s = 0
-    e = n-1
-    f = 0
-    while(s<=e):
-        M = (s+e)//2
-        if(a[M]<i):
-            s = M+1
-        elif(a[M]>i):
-            e = M-1
-        else:
-            f = 1
-            print(1)
-            break
-    if(f == 0):
+    if(i in map):
+        print(1)
+    else:
         print(0)
+    
