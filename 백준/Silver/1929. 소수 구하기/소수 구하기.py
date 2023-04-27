a1f1121d752a6e32a89566b051b07c7693066638
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-04-28 00:26:54 +0900

    [Silver III] Title: 소수 구하기, Time: 5552 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,14 @@
-m,n=map(int,input().split())
+import sys
+n,m =map(int, sys.stdin.readline().split())
 
-for i in range(m,n+1):
-    if i==1:
-        continue
-    for j in range(2,int(i**0.5)+1):
-        if i%j==0: 
-            break  
+for i in range(n,m+1):
+    if(i==1):
+        pass
     else:
-        print(i)
+        p=0
+        for j in range(2,int(i**0.5)+1):
+            if(i%j==0):
+                p=1
+                break
+        if(p==0):
+            print(i)
