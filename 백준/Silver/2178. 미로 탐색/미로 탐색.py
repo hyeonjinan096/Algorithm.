5b3d1f0bb575538d4eac1296f2ace47013931cf8
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-05-17 12:33:38 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,38 +1,34 @@
 import sys
-from collections import deque  #60m
+from collections import deque
 
-r,c = map(int,sys.stdin.readline().strip().split())
+r,c = map(int, sys.stdin.readline().strip().split())
 
-graph = []  
-visited = [[False]*c for _ in range(r)]
-
-for _ in range(r):
-   graph.append(list(map(int,sys.stdin.readline().strip()))) 
-
+graph = []
+visited=[[False]*c for _ in range(r)]
+for i in range(r):
+    graph.append(list(map(int,sys.stdin.readline().strip())))
 
 
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-dx =[1,-1,0,0]
-dy =[0,0,1,-1]
 visited[0][0]=True
-
-q= deque()
+q=deque()
 q.append((0,0,1))
 f=0
-
 while(q):
-    cx,cy,count= q.popleft()
+    cx,cy,count = q.popleft()
     for i in range(4):
-        nx = cx+dx[i]
+        nx =cx+dx[i]
         ny = cy+dy[i]
-        if(nx>=0 and nx<r and ny>=0 and ny<c):
-            if( not visited[nx][ny] and graph[nx][ny]== 1):  
-                if(nx==r-1 and ny==c-1):
+        if(nx>=0 and nx < r and ny>=0 and ny <c):
+            if(graph[nx][ny]==1 and not visited[nx][ny]):
+                if(nx == r-1 and ny ==c-1):
                     print(count+1)
                     f=1
                     break
-                    
                 visited[nx][ny]=True
                 q.append((nx,ny,count+1))
-    if(f==1) : break
-    
+    if(f==1):
+        break
+
