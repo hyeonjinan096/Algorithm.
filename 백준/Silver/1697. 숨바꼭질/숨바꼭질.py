4b70c1952b6073f2826f3e188a757439c98effd7
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-05-26 02:09:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35356 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,29 +1,21 @@
 import sys
 from collections import deque
-n,k = map(int,sys.stdin.readline().strip().split())
-visited = [-1] * 100001
+n, k = map(int,sys.stdin.readline().strip().split())
 
-def bsf(v):
-    q= deque()
-    q.append(v)
-    visited[v] = 0
+def bfs():
+    q = deque()             
+    q.append(n)           
+    while  q:
+        v = q.popleft()     
+        if v == k:
+            print(visited[v])
+            break
+        for nv in (v - 1, v + 1, v * 2):    
+            if 0 <= nv <= MAX and not visited[nv]:
+                visited[nv] = visited[v] + 1
+                q.append(nv)    
 
-    while(q): 
-        cur_v = q.popleft()
-        for i in range(3):
-            if(i==0):next_v =cur_v +1
-            elif(i==1):next_v = cur_v -1
-            else:next_v = cur_v*2
-            if(0<=next_v <= 100000 and visited[next_v] == -1):
-                q.append(next_v)
-                visited[next_v]= visited[cur_v]+1
-            if(next_v==k):
-                print(visited[next_v])
-                return 
-     
+MAX = 10 ** 5               # 시간초과 방지
+visited = [0] * (MAX + 1)      # 이동하는 시간 count
 
-bsf(n)
-
-            
-    
-    
+bfs()
