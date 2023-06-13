50897e568470b55f915268630530a1d4b0aed239
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-06-14 00:15:28 +0900

    [Silver V] Title: 단어 나누기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,13 +1,15 @@
-string = input()
+import sys
 
-answer = "z" * len(string)
+arr = sys.stdin.readline().strip()
 
-for i in range(1, len(string) - 1):
-    for j in range(i + 1, len(string)):
-        s1 = string[:i][::-1]
-        s2 = string[i:j][::-1]
-        s3 = string[j:][::-1]
-        new_string = s1 + s2 + s3
-        answer = min(answer, new_string)  
+A = "z"*len(arr)
 
-print(answer)
+for i in range(1,len(arr)-1):
+    for j in range(i+1,len(arr)):
+        a = arr[:i][::-1]
+        b = arr[i:j][::-1]
+        c = arr[j:][::-1]
+        B =a+b+c
+        if(A>B):A=B  #A =min(A,B)
+
+print(A)
