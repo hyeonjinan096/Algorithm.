92c8c881ea814e4836b42286d97e8cf390619f6e
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-06-14 13:07:14 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,14 +1,14 @@
 def solution(players, callings):
-    idx = {i: player for i ,player in enumerate(players)}
-    p = {player:i for i, player in enumerate(players)}
+    p = {p : i for i , p in enumerate(players)}
     
     for call in callings:
-        loc = p[call] #호명한 선수의 등수
-        loc2 = loc-1 #앞 선수 등수
+        idx = p[call] 
+    
+        a = players[idx-1]
+        players[idx-1] =players[idx]
+        players[idx] = a
         
-        idx[loc] = idx[loc2]
-        idx[loc2] = call
-        p[call] = loc2
-        p[idx[loc]] = loc
+        p[call] = idx-1
+        p[a] = idx
         
-    return list(idx.values())
+    return players
