02d12505577aa3810d22ea99f60e591c717384a3
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-06-15 12:04:18 +0900

    [Silver IV] Title: DNA, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -1,24 +1,21 @@
 import sys
 
 N,M = map(int,sys.stdin.readline().strip().split())
-arr = []
+arr =[]
 for i in range(N):
     arr.append(list(map(str,sys.stdin.readline().strip())))
-    
 
-answer=''
-sum =0
+answer=""
+sum = 0
 for i in range(M):
-    map ={"A":0, "C":0,"G":0 ,"T":0}
+    d={"A":0, "C":0, "G":0, "T":0}
     for j in range(N):
-        map[arr[j][i]] +=1
-    m = max(map,key=map.get)
+        d[arr[j][i]]+=1
+
+    m = max(d,key=d.get)    
     answer+=m
-    sum += (N- map[m])
-    map.clear
+    sum+=(N-d[m])
+
 
 print(answer)
 print(sum)
-
-
-#answer=[](append)와 answer=""(+=)의 차이를 알것
