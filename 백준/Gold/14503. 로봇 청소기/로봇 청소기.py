96ba7886b494f18d7edb437567b4fe3164502c2b
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-03 18:25:41 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31316 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,70 +1,45 @@
 import sys
 
-n,m = map(int,sys.stdin.readline().strip().split())
-x,y,z = map(int,sys.stdin.readline().strip().split())
+N,M = map(int, sys.stdin.readline().strip().split())
+x,y,z = map(int, sys.stdin.readline().strip().split())
 g =[]
-for _ in range(n):
+
+for i in range(N):
     g.append(list(map(int,sys.stdin.readline().strip().split())))
-visited=[[False]*m for _ in range(n)]
 
 
+count = 0
+dx = [-1,0,1,0]#0,1,2,3
+dy = [0,1,0,-1]
 
-def check(x,y):
-    #print("%d,%d"%(x,y))
-    if g[x][y] == 1:
-        return 1  #벽일때
-    if visited[x][y] :
-        return 2  #이미 청소했을 때
-    return 0
-
-
-
-
-
-def clean(x,y,z,count): #자리+방향
-    if(check(x,y)==0):
-        count+=1
-
-    #print("(%d,%d,%d) count = %d,"%(x,y,z,count))
-
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
-    visited[x][y] = True
-    flag = 0
-    if(flag==0):
-        Z = z
-        for _ in range(4):
-            Z -=1   #시계방향을 돌리기
-            if(Z<0): Z =3
-            if(check(x+dx[Z],y+dy[Z])==0):
-                #print("%d->%d 청소가능"%(z,Z))
-                clean(x+dx[Z],y+dy[Z],Z,count)
-                return 
-                #flag = 1
-                #break
-    if(flag==0):
-        #print("back")
-        if(z == 0):b=2
-        elif(z==2):b=0
-        elif(z==3):b=1
-        elif(z==1):b=3 #후진하기 b = 후진방향
-        if check(x-dx[z],y-dy[z])!=1: # 벽이 아니면 후진
-            #print("%d->%d 후진"%(z,b))
-            clean(x+dx[b],y+dy[b],z,count)
-            return 
-        else: #벽이면 중지
-            #print("멈춤!")
-            print(count)
-            return 
-        
+def clean(x,y,z):
+    global count
     
-        
+    #청소 
+    if(g[x][y]==0):
+        g[x][y] = 2
+        count +=1
+    
+    #탐색
+    Z= z
+    #청소할 방이 있는가
+    for i in range(4):
+        Z = (Z+3) % 4
+        if(g[x+dx[Z]][y+dy[Z]]==0):
+            clean(x+dx[Z],y+dy[Z],Z)
+            return
+    #후진이 가능한가
+    if(g[x-dx[z]][y-dy[z]]!=1):
+        clean(x-dx[z],y-dy[z],z)
+        return 
+    else:
+        return count
 
 
+clean(x,y,z)
+print(count)
 
 
-clean(x,y,z,0)
-
-
-
+    
+    
 
