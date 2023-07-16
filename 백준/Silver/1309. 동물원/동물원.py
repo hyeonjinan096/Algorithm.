dfe554e9b8641bb4c8e82b62994b8c1ece3e5ef0
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-16 23:07:16 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,15 +1,5 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-dp = [0 for i in range(N+1)]
-
-if(N > 1) :
-    dp[1] = 3
-    dp[2] = 7
-    for i in range(3,N+1):
-        dp[i] = (dp[i-1]*2 + dp[i-2])%9901
-elif(N == 1):
-    dp[N]=3
-    
-print(dp[N])
+n = int(input())
+dp = [1,3] + [0]*(n-1)
+for i in range(2,n+1):
+    dp[i] = (dp[i-2] + dp[i-1]*2)%9901
+print(dp[n])
