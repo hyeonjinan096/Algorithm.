f4f4e303c9b7d153553a391ba385c1f8fdfb25fe
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-19 02:51:27 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.75 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,11 +1,8 @@
 function solution(A,B){
-    var answer = [];
-    A = A.sort((a,b)=>a-b);  //배열 오름차순 정렬
-    B = B.sort((a,b)=>b-a);  //배열 내림차순 정렬
-    for(let i = 0;i<A.length;i++){
-       answer.push(A[i]*B[i]);  //배열에 추가 하기 push
-    }
-    result = answer.reduce((sum,cv) =>sum+cv,0);  //reduce ->sum . avg 가능 
+    var answer = 0;
+    A = A.sort((a,b)=>a-b);
+    B = B.sort((a,b)=>b-a);
     
-    return result;
+    
+    return A.map((a,i)=> a*B[i]).reduce((s,cv)=>s+cv,0);
 }
