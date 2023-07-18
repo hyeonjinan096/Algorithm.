7a8194a445c2e427765a35dc8485d0be6bbc8d90
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-19 01:30:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 606.49 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,27 +1,16 @@
 function solution(e) {
-    let s = [];
+    let len = e.length;
     let set = new Set();
-    let k;
-    let sum =e.reduce((sum,cv)=>sum+cv,0);
+    let sum = e.reduce((s,cv)=>s+cv,0)
     set.add(sum);
-    let len = e.length
+    e = e.concat(e);
+    console.log(e);
     
-    for(let i = 1 ; i <= parseInt(len/2);i++){
-        for(let j=0; j<len;j++){
-            if(i+j <len){ k = (e.slice(j,(j+i)).reduce((sum,cv)=>sum+cv,0));}
-            else{
-                 k = (e.slice(j).concat(e.slice(0, j+i-len)).reduce((sum,cv)=>sum+cv,0));
-                
-            }
-            set.add(k);
-            set.add(sum-k);
-            
-            //if(s.findIndex((a)=>a==k)==-1){s.push(k);}
-            //if(s.findIndex((a)=>a==(sum-k))==-1){s.push(sum-k);}
-            
+    for(let i=1;i<=parseInt(len/2);i++){
+        for(let j=0;j<len;j++){
+            set.add((e.slice(j,j+i)).reduce((sum,cv)=>sum + cv,0));
+            set.add(sum - (e.slice(j,j+i)).reduce((sum,cv)=>sum + cv,0));
         }
     }
-    
-    
     return set.size;
 }
