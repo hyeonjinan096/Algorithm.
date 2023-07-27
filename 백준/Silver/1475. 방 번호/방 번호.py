d64638bc720ef02d83acb88d5ef89a231d0e15b1
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-27 23:07:26 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,13 @@
+num = input()
+checked = [0]*10
+
+for i in num:
+    if i == '6' or i == '9':
+        if checked[6] <= checked[9]:
+            checked[6] += 1
+        else:
+            checked[9] += 1
+    else:
+        checked[int(i)] += 1
+
+print(max(checked))
