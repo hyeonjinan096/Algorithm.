6216775de1576cadfb82e6be05680a69be52e882
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-29 20:31:15 +0900

    [level 2] Title: 피로도, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,26 +1,19 @@
-//경로탐색#네트워크#조합만들기🌰
-//dfs🌰#bfs
-//visited
-//재귀함수
 function solution(k, dungeons) {
-    let d = dungeons;
-    let visited = Array(d.length).fill(0) //1.visited만들기
-    let answer = 0;
+    var answer = [];
+    let visited =Array(dungeons.length).fill(0);
+    
     function dfs(k,result){
-        answer = answer <result ? result: answer;
-        
-        for(let i in d){
-            if(k>=d[i][0] && visited[i] ==0 ){
-                visited[i] =1;
-                dfs(k-d[i][1],result+1);
+        answer =answer<result? result : answer;
+        for(let i in dungeons){
+            if(visited[i]==0 && k >= dungeons[i][0]){
+                visited[i] = 1;
+                dfs(k-dungeons[i][1],result+1)
                 visited[i] =0;
             }
+            
         }
-        
         return answer;
     }
     
-    
-    
-    return dfs(k, 0);
+    return (dfs(k,0));
 }
