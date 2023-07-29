e671b4461eb6066d98423c0cf8affed6d315c798
A239_HYEON <107539614+hyeonjinan096@users.noreply.github.com>
2023-07-29 21:43:01 +0900

    [level 2] Title: 구명보트, Time: 34.86 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,7 +1,6 @@
 function solution(people, limit) {
-    //let visited= Array(people.length).fill(0);
+    
     people.sort((a,b)=>b-a);
-    let length = people.length;
     let count =0;
     
     for(let i in people){
