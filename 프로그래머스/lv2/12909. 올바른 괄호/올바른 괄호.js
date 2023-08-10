09bc31c2c37aaa066fe245ec34021b98466acdc2
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-10 21:44:11 +0900

    [level 2] Title: 올바른 괄호, Time: 5.24 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,12 @@
-//복습2
 function solution(s){
-    
-    let stack =0;
+    var answer = true;
+    let q =0;
     for(let i of s){
-        i === "(" ? stack++ : stack--;
-        if(stack<0){return false;}
+        if(i=="("){q++;}
+        else{q--;}
+        if(q<0){return false;}
     }
-    return stack ==0 ? true : false;
-    
-    //if(stack == 0){return true;}
-    //else{return false;} 왜 true가 앞이면 시간초과인지 모르겠음;;
-    
-    
+    if(q==0){return true;}
+    else{return false;}
+    return answer;
 }
