20fec621f56fa1f9e1292219fade7fe0a83d08c8
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-11 02:39:38 +0900

    [level 2] Title: 기능개발, Time: 2.90 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,21 +1,19 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let list =[];
-    let k;
-    for(let i in progresses){
-        
-        k = ((100-progresses[i])/speeds[i]);
-        if(parseInt(k)!=k){k = parseInt(k)+1;}
-        list.push(k);
+    let li =[];
+    let day ;
+    for(let i in speeds){
+        day = (100-progresses[i])/speeds[i];
+        if(parseInt(day) != day){day = parseInt(day)+1;}
+        li.push(day); 
     }
-    //console.log(list);
-    let min = list[0];
+    console.log(li);
+    let min =li[0];
     let sum =0;
-    for(let i of list){
-        if(min<i){answer.push(sum);sum =0;min= i;}
+    for(let i of li){
+        if(min<i){answer.push(sum);sum =0;min =i;}
         sum++;
     }
     answer.push(sum);
     return answer;
 }
-//console.log(solution(	[95, 90, 99, 99, 80, 99,80], [1, 1, 1, 1, 1, 1,1]));
