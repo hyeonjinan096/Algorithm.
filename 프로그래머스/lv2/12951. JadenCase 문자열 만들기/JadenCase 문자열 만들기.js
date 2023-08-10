8559105d4521cbf1dfe5f639cf3dd9d3ef4663a9
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-10 21:26:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,12 +1,13 @@
 function solution(s) {
     var answer = '';
-    let li = s.split("")
-    
-    li=li.map((a,i)=>{
-        if(i==0 || li[i-1]==' '){
-            return a.toUpperCase();}
-        else{return a.toLowerCase();}
-        });
-    
-    return li.join("");
+    let k =0;
+    S= s.split('');
+    answer=S.map((e)=>{
+        if(k==0 && e.toLowerCase() == e){e=e.toUpperCase();}
+        else if(k==1 && e.toUpperCase()==e){e=e.toLowerCase();}
+        k=1;
+        if(e==' '){k=0;}
+        return e;
+    })
+    return answer.join("");
 }
