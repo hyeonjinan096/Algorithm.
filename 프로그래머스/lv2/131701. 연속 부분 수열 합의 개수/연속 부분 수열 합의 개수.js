d92de05506a9ecf5acdd4b37819ee022b28f024c
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-11 00:43:25 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1103.85 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,16 +1,19 @@
-function solution(e) {
-    let len = e.length;
+function solution(elements) {
+    var answer = 0;
     let set = new Set();
-    let sum = e.reduce((s,cv)=>s+cv,0)
-    set.add(sum);
-    e = e.concat(e);
-    console.log(e);
+    let E = elements+"," + elements;
+    let k;
+    E = E.split(",");
+    E = E.map((e)=>parseInt(e));
+    let sum;
     
-    for(let i=1;i<=parseInt(len/2);i++){
-        for(let j=0;j<len;j++){
-            set.add((e.slice(j,j+i)).reduce((sum,cv)=>sum + cv,0));
-            set.add(sum - (e.slice(j,j+i)).reduce((sum,cv)=>sum + cv,0));
+    for(let i in elements){
+        for(let j in elements){
+            k = E.slice(j,parseInt(i)+parseInt(j)+1)
+            sum = (k.reduce((s,c)=>s+c,0));
+            set.add(sum);
         }
     }
+    
     return set.size;
 }
