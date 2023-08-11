ee64b375456d4c8cb1ca1f8cbac436f7536f6367
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-12 00:05:43 +0900

    [level 2] Title: 소수 찾기, Time: 1.17 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,43 +1,38 @@
 function solution(numbers) {
     var answer = 0;
-    let set = new Set();
+    let flag =0;
     let visited = Array(numbers.length).fill(0);
-  
-    
-    
-    let Num = Array.from(numbers);
-    Num =Num.map((e)=>parseInt(e)) //문자열-> 숫자 배열로 변환
-    
+    let set = new Set();
     function check(n){
-        //let k = n>2 ? n/2 : n;
-        for(let i =2 ;i<=Math.sqrt(n) ;i++){
-            if(n%i ==0){return 0;}
+        if(n == 1){return false;}
+        for(let i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){return false;}
         }
-        if(n ==1 ||n==0){return 0;}
-        return 1;
-    }
+        return true;
+    }//소수 판별 함수
     
-    
-    function dfs(num,y){
-        if(!set.has(num)){
-            if(check(num)){
-                answer++;}
-            set.add(num);}
-        for(let i in Num){
+    function dfs(k){
+        set.add(parseInt(k));
+
+        for(let i in numbers){
             if(visited[i]==0){
                 visited[i] =1;
-                dfs(num+(y*10)*Num[i],y*10);
+                dfs(k+numbers[i]);
                 visited[i] =0;
             }
         }
-        return;
     }
     
-    for(let i in Num){
-        visited[i] = 1;
-        dfs(Num[i],1);
-        visited[i] = 0;
+    for(let i in numbers){
+        if(numbers[i] != '0'){
+            visited[i] =1;
+            dfs(numbers[i]);
+            visited[i] =0;
+        }
     }
     
+    for(let i of set){
+        if(check(i)){answer++;}
+    }
     return answer;
 }
