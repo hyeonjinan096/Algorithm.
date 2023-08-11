0ab3652af39600d814e0e743195405a7d1c32da7
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-11 20:01:35 +0900

    [level 2] Title: 타겟 넘버, Time: 17.30 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,15 @@
 function solution(numbers, target) {
     var answer = 0;
+    let arr = [];
     
-    function dfs(N,result,p){
-        result = result +numbers[N]*p
-        if(N == numbers.length-1){
-            if(result ==target){answer++;}
-            return;}
-        dfs(N+1,result,1);
-        dfs(N+1,result,-1);
-        
-        
+    function dfs(k,i){
+        if(i == numbers.length){
+            if(k == target){answer++;}
+            return;
+        }
+        dfs(k-numbers[i],i+1);
+        dfs(k+numbers[i],i+1);
     }
-    dfs(0,0,1);
-    dfs(0,0,-1);
-    
+    dfs(0,0);
     return answer;
 }
