0f865459bda898a1c58a0be05890a192da3e35da
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-11 21:17:09 +0900

    [level 2] Title: 모음 사전, Time: 0.68 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/README.md
+++ 프로그래머스/lv2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 0.73 ms
+메모리: 33.7 MB, 시간: 0.68 ms
 
 ### 구분
 
--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,23 +1,18 @@
 function solution(word) {
     var answer = 0;
-    let arr=[];
-    let li = ['A','E','I','O','U'];
-    let result;
-    function dfs(i,n,arr){
-        answer++;
-        if(arr == word){
-            result=answer;
-            //console.log(answer);
+    let count =0;
+    let w = ['A','E','I','O','U'];
+    function dfs(s){
+        count++;
+        if(s == word){answer = count; return;}
+        if(s.length == 5){return;}
+        for(let i of w){
+            dfs(s+i);
         }
-        if(n>4){return;}
-        for(let j of li){
-            dfs(j,n+1,arr+j);
-            i++;
-        }
+        
     }
-    
-    for(let i of li){
-        dfs(i,1,i);
+    for(let i of w){
+        dfs(i); 
     }
-    return result;
+    return answer;
 }
