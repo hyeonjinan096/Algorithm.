cade4d5a5f9531520f082301b3ba000148ebb219
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-12 09:30:00 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,14 +1,11 @@
 function solution(clothes) {
-    var answer = 0;
-    let object = {};
-    
+    var answer = 1;
+    let object ={};
     for(let i of clothes){
         object[i[1]] = (object[i[1]]||0)+1;
     }
-    let sum=1;
     for(let i in object){
-        sum *=(object[i]+1)
+        answer*=(object[i]+1);
     }
-    
-    return sum-1;
+    return answer-1;
 }
