53386bad568de9603ac0bbf193774a007d92eb7e
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-08-22 21:45:08 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,11 +1,11 @@
 function solution(n) {
     var answer = 0;
-    let li = new Array(n+1).fill(0);
-    li[1] = 1;
-    li[2] = 2;
-    for(let i =3;i<=n;i++){
-        li[i] = (li[i-1]+li[i-2])% 1234567;
+    let arr = new Array(n+1).fill(0);
+    arr[1] =1;
+    arr[2] =2;
+    
+    for(let i=3;i<=n;i++){
+        arr[i] = arr[i-1]%1234567+arr[i-2]%1234567;
     }
-
-    return li[n] % 1234567;
+    return arr[n]%1234567;
 }
