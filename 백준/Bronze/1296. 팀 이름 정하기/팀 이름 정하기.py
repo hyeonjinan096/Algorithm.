bf4370320b4144e852f54e557ec88ce4bc9e473e
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-09-01 23:38:30 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -1,18 +1,17 @@
 name = input()
-numOfTeam = int(input())
-teamList = []
-probList = []
-teamIndex = 0
-maxPorb = 0;
-for i in range(numOfTeam):
-    teamList.append(input())
-    L = name.count("L") + str(teamList[i]).count("L")
-    O = name.count("O") + str(teamList[i]).count("O")
-    V = name.count("V") + str(teamList[i]).count("V")
-    E = name.count("E") + str(teamList[i]).count("E")
-    num = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100
-    probList.append((num, teamList[i]))
-    
-probList.sort(key=lambda x : (-x[0], x[1]))
+N = int(input())
+list = []
+A = []
 
-print(probList[0][1])
+for i in range(N):
+    list.append(input())
+    L = name.count("L") + str(list[i]).count("L")
+    O = name.count("O") + str(list[i]).count("O")
+    V = name.count("V") + str(list[i]).count("V")
+    E = name.count("E") + str(list[i]).count("E")
+    num = ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100
+    A.append((num, list[i]))
+    
+A.sort(key=lambda q : (-q[0], q[1]))
+
+print(A[0][1])
