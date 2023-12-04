9d3f16d161d104dd927bfbd40e4b63a060fb308a
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-12-04 22:07:37 +0900

    [Silver IV] Title: ACM-ICPC, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11946. ACM－ICPC/ACM－ICPC.py
+++ 백준/Silver/11946. ACM－ICPC/ACM－ICPC.py
@@ -1,21 +1,27 @@
-n, m, q = map(int,input().split())
-dic = {}
-for i in range(1, n+1):
-	for j in range(1, m+1):
-		dic[(i, j)] = [0, -1]
+n, m, q = map(int, input().split())
+
+problems = {}
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        problems[(i, j)] = [0, -1]  
+
 for _ in range(q):
-	log = input().split()
-	if log[3] != 'AC':
-		dic[(int(log[1]), int(log[2]))][0] += 1
-	else:
-		if dic[(int(log[1]), int(log[2]))][1] < 0:
-			dic[(int(log[1]), int(log[2]))][1] = dic[(int(log[1]), int(log[2]))][0] * 20 + int(log[0])
-dab = [[i,0,0] for i in range(1, n+1)]
-for i in range(1, n+1):
-	for j in range(1, m+1):
-		if dic[(i,j)][1] >= 0:
-			dab[i-1][1] += 1
-			dab[i-1][2] += dic[(i,j)][1]
-dab.sort(key = lambda x: (-x[1], x[2], x[0]))
-for i in dab:
-	print(*i)
+    log = input().split()
+    problem_key = (int(log[1]), int(log[2]))
+    if log[3] != 'AC':
+        problems[problem_key][0] += 1
+    elif problems[problem_key][1] < 0:
+        problems[problem_key][1] = problems[problem_key][0] * 20 + int(log[0])
+
+results = [[i, 0, 0] for i in range(1, n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if problems[(i, j)][1] >= 0:
+            results[i - 1][1] += 1
+            results[i - 1][2] += problems[(i, j)][1]
+
+results.sort(key=lambda x: (-x[1], x[2], x[0]))
+
+for result in results:
+    print(*result)
