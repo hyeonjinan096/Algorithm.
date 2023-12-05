20a6c57d868a5b6d89b3068a8dc6913a30176e09
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-12-06 02:39:33 +0900

    [Silver V] Title: 욱제는 도박쟁이야!!, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/14655. 욱제는 도박쟁이야！！/욱제는 도박쟁이야！！.py
@@ -0,0 +1,12 @@
+N = int(input())    
+first = list(map(int, input().split()))  
+second = list(map(int, input().split()))
+ 
+sum = 0
+for i in first:
+    sum += abs(i)
+ 
+for j in second:
+    sum += abs(j)
+ 
+print(sum)
