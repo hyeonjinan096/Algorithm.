e538e2c4b2b0ec595d70534417f55e956b6bebee
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2023-12-25 02:23:58 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(N):
+    A = list(map(int,input().split()))
+    count = 0
+    avg = sum(A[1:])/A[0]
+    for j in range(1,len(A)):
+        if (A[j] > avg):
+            count +=1
+    prob = format((count/A[0])*100,".3f")
+    print(str(prob)+"%")
