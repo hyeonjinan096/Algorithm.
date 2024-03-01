5e38b194132b57b54ea808af68e4a70907827329
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-03-02 04:24:04 +0900

    [Silver IV] Title: 올바른 배열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+arr = sorted(arr)
+temp = []
+
+for i in range(0, n):
+  cnt = 0
+  for j in range(arr[i], arr[i]+5):
+    if j not in arr:
+      cnt += 1
+  temp.append(cnt)
+print(min(temp))
