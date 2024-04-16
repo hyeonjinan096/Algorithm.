9d670179afc861e75e542d190e90b372c6d7eb6b
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-04-16 17:46:49 +0900

    [Silver III] Title: 마라톤 1, Time: 328 ms, Memory: 50332 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.py
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.py
@@ -1,22 +1,20 @@
 from sys import stdin
-n = int(stdin.readline())
-checks = []
 
-for _ in range(n):
-    x, y = map(int, stdin.readline().split())
-    checks.append([x, y])
+N = int(input())
+point = [list(map(int, stdin.readline().split())) for _ in range(N)]
+res = float('inf')
+sum = 0
 
-total = 0
+def distance(a,b):
+    return abs(point[a][0]-point[b][0]) + abs(point[a][1]-point[b][1])
 
-for i in range(n-1):
-    total += abs(checks[i][0] - checks[i+1][0]) + abs(checks[i][1] - checks[i+1][1])
+for i in range(N-1):
+    sum += distance(i, i+1)
 
-maxDis = 0
-for i in range(1, n-1):
-    prev_to_mid = abs(checks[i][0]-checks[i-1][0])+abs(checks[i][1]-checks[i-1][1])
-    mid_to_end = abs(checks[i+1][0]-checks[i][0])+abs(checks[i+1][1]-checks[i][1])
-    prev_to_end = abs(checks[i+1][0]-checks[i-1][0])+abs(checks[i+1][1]-checks[i-1][1])
-    val = prev_to_mid + mid_to_end - prev_to_end
-    maxDis = max(maxDis, val)
+for i in range(1,N-1):
+    tmp = sum 
+    tmp -= distance(i, i+1) + distance(i, i-1)
+    tmp += distance(i-1, i+1)
+    res = min(res, tmp)
 
-print(total-maxDis)
+print(res) 
