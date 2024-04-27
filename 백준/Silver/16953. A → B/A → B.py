71d18f678b43576ae7d62d49e019a1d2b69b546b
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-04-27 17:22:42 +0900

    [Silver II] Title: A → B, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,19 +1,17 @@
 a,b = map(int, input().split())
 
-answer = []
+answer = 0
 
 def dfs(cur_v,l):
     global answer
     if cur_v > b:
         return
     if cur_v == b:
-        answer.append(l)
+        answer = l
         return
     dfs(cur_v*2,l+1)
     dfs(cur_v*10+1,l+1)
 
 dfs(a,1)
-if len(answer) ==0:
-    print(-1)
-else:
-    print(min(answer))
+
+print(answer or -1)
