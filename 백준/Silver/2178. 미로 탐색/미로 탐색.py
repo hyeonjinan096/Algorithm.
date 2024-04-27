7ed24917357172d8a2d7aa6bcedf6b9c3f88a3bc
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-04-28 00:18:08 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,27 +1,23 @@
 from collections import deque
 
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-cnt = 0
-
-def bfs(x, y):
-    q = deque([(x,y)])
-    visited[x][y] = 1
-
-    while q:
-        x, y = q.popleft()
+visited = [[0]*m for _ in range(n)]
+x = [0,0,1,-1]
+y = [1,-1,0,0]
+def bfs(start_x, start_y):
+    visited[start_x][start_y] = 1
+    q = deque()
+    q.append((start_x,start_y))
+    while(q):
+        cur_x, cur_y = q.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-			
-            # if문 하나에서 여러 조건을 확인하는 것 보다 아래처럼 쪼개는 것이 속도가 빠름
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1: 
-                if visited[nx][ny] == 0:
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
-    return
+            next_x = cur_x + x[i]
+            next_y = cur_y + y[i]
+            if 0 <= next_x < n and 0 <= next_y <m:
+                if visited[next_x][next_y] == 0 and graph[next_x][next_y] == 1:
+                    visited[next_x][next_y] = visited[cur_x][cur_y] +1
+                    q.append((next_x,next_y))
 bfs(0,0)
 print(visited[n-1][m-1])
