673b73d444c98fc4e4f82a13aa34818d2c0582a2
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-05-16 21:58:11 +0900

    [D3] Title: 햄버거 다이어트, Time: 899 ms, Memory: 63,520 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -21,37 +21,34 @@ print(c, d, e)                          실수형 변수 3개 출력하는 예�
 print(f)                                문자열 1개 출력하는 예제
 '''
 
-
-
 '''
-      아래의 구문은 input.txt 를 read only 형식으로 연 후,
-      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	  아래의 구문은 input.txt 를 read only 형식으로 연 후,
+	  앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	  여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	  아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
 
-      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+	  따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+	  아래 구문을 사용하기 위해서는 import sys가 필요합니다.
 
-      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+	  단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
 '''
-#sys.stdin = open("input.txt", "r")
-from itertools import combinations
+# sys.stdin = open("input.txt", "r")
+
 T = int(input())
-def dfs(i,c,s):
-    global answer
-    if c<=C:
-        answer = max(answer,s)
-    else: 
-        return
-    for j in range(i+1,n):
-        dfs(j,c+lst[j][1],s+lst[j][0])
-
-
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+def dfs(i,t,c):
+	global answer
+	if c<=l:
+		answer = max(answer,t)
+	if c>l:
+		return
+	for j in range(i+1,n):
+		dfs(j,t+values[j][0], c+values[j][1])
+
 for test_case in range(1, T + 1):
-    n, C = map(int,input().split())
-    lst = [list(map(int,input().split())) for _ in range(n)]
-    answer=0
-    for i in range(n):
-        dfs(i,lst[i][1],lst[i][0])
-    print(f'#{test_case} {answer}')
+	n, l = map(int,input().split())
+	values = [list(map(int,input().split())) for _ in range(n)]
+	answer =0
+	for i in range(n):
+		dfs(i,values[i][0],values[i][1])
+	print(f'#{test_case} {answer}')
