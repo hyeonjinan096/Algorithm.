0cd5654fa890ca57566b2d94f61f1a987bba51ed
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-09-26 20:44:23 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.15 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,35 +1,40 @@
 from math import ceil
-
 def solution(fees, records):
-    answer = []
-    b_time, b_pay, p_time, p_pay = fees
-    parkings = {}
-    results = {}
-    last_time = 23*60+59
+    answer=[]
+    parking = {}
+    result = {}
+    [ b_t, b_p, p_t, p_p]= fees
     
     for record in records:
         time, car, io = record.split()
-        h, m = map(int,time.split(':'))
-        t = h* 60 + m
+        h, m = map(int,time.split(":"))
+        t = h*60 + m
         
         if io == 'IN':
-            parkings[car] = t
+            parking[car] = t
         else:
-            if car in results:
-                results[car] += t-parkings[car]
+            if car not in result:
+                result[car] = t-parking[car]
             else:
-                results[car] = t-parkings[car]
-            del parkings[car] #확인하기
-            
-    for car, t in parkings.items():
-        if car in results:
-            results[car] += last_time - t
+                result[car]+= t-parking[car]
+            del parking[car]
+    
+    for car, t in parking.items():
+        if car not in result:
+            result[car] = (23*60+59) - t
         else:
-            results[car] = last_time - t
-    
-    results = sorted(results.items(), key=lambda  x:x[0])
-    
-    for car, t in results:
-        answer.append(b_pay + max(0,ceil((t-b_time)/p_time))*p_pay)
-
+            result[car] += (23*60+59) - t
+            
+    result = sorted(result.items(), key=lambda x:x[0])
+    for car, t in result:
+        print(car, t)
+        answer.append(b_p + ceil(max(0,(t-b_t))/p_t)*p_p)
+        
     return answer
+
+#records -> time car io -> (time -> h,m -> t) 1,000
+#io: i -> parking 데이터
+#io: o -> time - car의 paking 데이터  #result 담기 /없으면 갱신, 있으면 추가 1,000
+#parking -> last_time - parking데이터 
+ 
+# result 차 번호 작은 거 부터 정렬 
