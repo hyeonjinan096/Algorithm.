6dc19877c8ed2a734592faa8dbc113fae86c986f
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-01 17:01:05 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.25 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,40 +1,34 @@
-from math import ceil
+import math
+
 def solution(fees, records):
-    answer=[]
+    answer = []
+    b_t, b_m, p_t,p_m = fees
     parking = {}
     result = {}
-    [ b_t, b_p, p_t, p_p]= fees
-    
+    last_time = 23*60 + 59
     for record in records:
         time, car, io = record.split()
-        h, m = map(int,time.split(":"))
+        h,m = map(int,time.split(':'))
         t = h*60 + m
         
-        if io == 'IN':
+        if io == "IN":
             parking[car] = t
         else:
             if car not in result:
-                result[car] = t-parking[car]
-            else:
-                result[car]+= t-parking[car]
+                result[car] = 0
+            result[car] += t-parking[car]
             del parking[car]
     
     for car, t in parking.items():
         if car not in result:
-            result[car] = (23*60+59) - t
-        else:
-            result[car] += (23*60+59) - t
-            
-    result = sorted(result.items(), key=lambda x:x[0])
+            result[car] = 0
+        result[car] += last_time-parking[car]
+    
+    result = sorted(result.items(), key = lambda x:x[0])
+  
+    answer = []
     for car, t in result:
-        print(car, t)
-        answer.append(b_p + ceil(max(0,(t-b_t))/p_t)*p_p)
+        answer.append(b_m + (math.ceil(max(0,t-b_t)/p_t))*p_m)
         
+    
     return answer
-
-#records -> time car io -> (time -> h,m -> t) 1,000
-#io: i -> parking 데이터
-#io: o -> time - car의 paking 데이터  #result 담기 /없으면 갱신, 있으면 추가 1,000
-#parking -> last_time - parking데이터 
- 
-# result 차 번호 작은 거 부터 정렬 
