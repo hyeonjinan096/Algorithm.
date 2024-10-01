3579c906dff03e7187cb8c134f64b267c10e6644
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-01 17:47:55 +0900

    [level 3] Title: 정수 삼각형, Time: 101.78 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,17 @@
-import copy
 def solution(triangle):
-    answer = copy.deepcopy(triangle)
-    for x in range(len(triangle)-1):
-        for y in range(len(triangle[x])): 
-            if triangle[x+1][y] + answer[x][y] > answer[x+1][y]:
-                answer[x+1][y] =  triangle[x+1][y] + answer[x][y]
-            
-            if triangle[x+1][y+1] + answer[x][y] > answer[x+1][y+1]:
-                answer[x+1][y+1] =  triangle[x+1][y+1] + answer[x][y]
-            
-        
+    answer = 0
     
-    return max(answer[-1])
+    for i in range(1,len(triangle)):
+        for j in range(len(triangle[i])):
+            tmp = triangle[i][j] 
+            if 0<=j-1<len(triangle[i-1]):
+                triangle[i][j] = max(triangle[i][j],tmp +triangle[i-1][j-1])
+            if 0<=j<len(triangle[i-1]):
+                triangle[i][j] = max(triangle[i][j],tmp +triangle[i-1][j])
+                    
+    
+    return max(triangle[-1])
+
+
+                    
+    
