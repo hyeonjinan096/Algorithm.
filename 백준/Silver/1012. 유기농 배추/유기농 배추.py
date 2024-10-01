faa1dd9e2632d48288d5f6ad1451f2230a75b5ad
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-01 18:48:33 +0900

    [Silver II] Title: 유기농 배추, Time: 196 ms, Memory: 31484 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,37 +1,29 @@
 import sys
 sys.setrecursionlimit(10000)
 
-import sys
-
 T = int(input())
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-def dfs(x,y):
-  graph[x][y] = 2
-  for i in range(4):
-    cur_x, cur_y =x+dx[i],y+dy[i]
-    if 0<=cur_x<M and 0<=cur_y<N and graph[cur_x][cur_y]==1:
-      graph[x][y] = 2
-      dfs(cur_x,cur_y)
-  return
-    
-
-
+dx=[0,0,-1,1]
+dy = [1,-1,0,0]
 
 for _ in range(T):
+  answer =0
   M,N,K = map(int,input().split())
-  graph = [[0]*N for _ in range(M)]
-  answer = 0
-
-  for j in range(K):
-    x,y = map(int,input().split())
+  graph = [[0]*M for _ in range(N)]
+  for _ in range(K):
+    y,x = map(int,input().split())
     graph[x][y] = 1
   
-  for i in range(M):
-    for j in range(N):
-      if graph[i][j] == 1:
-        dfs(i,j)
-        answer+=1
+  def dfs(x,y):
+    graph[x][y] = 2
+    for i in range(4):
+      if 0<=x+dx[i]<N and 0<=y+dy[i]<M:
+        if graph[x+dx[i]][y+dy[i]] == 1:
+          dfs(x+dx[i], y+dy[i])
 
+  for i in range(N):
+    for j in range(M):
+      if graph[i][j] ==1:
+        dfs(i,j)
+        answer +=1
   print(answer)
+      
