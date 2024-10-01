4dc05cc53341595ced890b12d33152d72ead44a3
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-01 18:20:03 +0900

    [level 3] Title: 징검다리 건너기, Time: 411.94 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,21 +1,22 @@
 def solution(stones, k):
     answer = 0
-    left = 1
-    right = 200000000
-    while left <= right:
-        mid = (left + right)//2
-        cnt = 0 
-        for i in stones:
-            tmp = i-mid
-            if tmp <=0:
+    Max = max(stones)
+    s, e = 0,Max
+    
+    while(s<=e):
+        m = (s+e)//2
+        cnt = 0
+        for j in stones:
+            if j-m <= 0:
                 cnt+=1
             else:
-                cnt = 0 
+                cnt =0
             if cnt >= k:
                 break
-        if cnt >= k:
-            right = mid-1
-        else: 
-            left = mid + 1
-    
-    return left
+        if cnt >=k:
+            e = m-1
+        if cnt < k:
+            s = m+1
+            
+                
+    return s
