5ad1fcb277d7a9a478d76a55c68847e84da8bf05
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-01 17:34:23 +0900

    [level 3] Title: 기둥과 보 설치, Time: 2897.07 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,28 +1,34 @@
 def solution(n, build_frame):
-    result = []
+    answer = [[]]
+    build = []
     
-    def check(result):
-        for x,y,a in result:
-            if a==0:
-                if y!=0 and [x,y-1,0] not in result and [x,y,1] not in result and [x-1,y,1] not in result:
+    def isOk():
+        nonlocal build
+        for x,y,a in build:
+            if a== 0:
+                if y != 0 and [x,y,1] not in build and [x-1,y,1] not in build and [x,y-1,0] not in build:
                     return False
             else:
-                if [x,y-1,0] not in result and [x+1,y-1,0] not in result and not ([x-1,y,1] in result and [x+1,y,1] in result):
+                if [x,y-1,0] not in build and [x+1,y-1,0] not in build and not ([x-1,y,1] in build and [x+1,y,1] in build):
                     return False
         return True
+                    
+        
     
     for x,y,a,b in build_frame:
         if b == 1:
-            result.append([x,y,a])
-            if not check(result):
-                result.remove([x,y,a])
+            build.append([x,y,a])
+            if not isOk():
+                build.remove([x,y,a])
         else:
-            if [x,y,a] in result:
-                result.remove([x,y,a])
-            if not check(result):
-                result.append([x,y,a])
-        
-    result.sort(key=lambda x:(x[0],x[1],x[2]))
+            if [x,y,a] not in build:
+                continue
+            build.remove([x,y,a])
+            if not isOk():
+                build.append([x,y,a])
+                
+    build.sort(key=lambda x:(x[0],x[1],x[2]))
     
-    return result
+    
+    return build
 
