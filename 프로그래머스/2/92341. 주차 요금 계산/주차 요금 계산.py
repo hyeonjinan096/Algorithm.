ce1e27c62b150e09107e7de12a882034e6440ccb
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-03 14:13:08 +0900

    [level 2] Title: 주차 요금 계산, Time: 2.96 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,34 +1,34 @@
 import math
-
 def solution(fees, records):
     answer = []
-    b_t, b_m, p_t,p_m = fees
+    b_t, b_f, p_t,p_f = fees
     parking = {}
     result = {}
-    last_time = 23*60 + 59
+    last_time = 23*60+59
+    
     for record in records:
-        time, car, io = record.split()
-        h,m = map(int,time.split(':'))
-        t = h*60 + m
-        
-        if io == "IN":
-            parking[car] = t
+        t, car, io = record.split()
+        h,m = map(int,t.split(':'))
+        time = h*60+m
+        if io == 'IN':
+            parking[car] = time
         else:
-            if car not in result:
-                result[car] = 0
-            result[car] += t-parking[car]
+            if car in result:
+                result[car] += time-parking[car]
+            else:
+                result[car] = time-parking[car]
             del parking[car]
-    
-    for car, t in parking.items():
-        if car not in result:
-            result[car] = 0
-        result[car] += last_time-parking[car]
-    
-    result = sorted(result.items(), key = lambda x:x[0])
-  
-    answer = []
-    for car, t in result:
-        answer.append(b_m + (math.ceil(max(0,t-b_t)/p_t))*p_m)
         
+    for car, time in parking.items():
+        if car in result:
+            result[car] += last_time-parking[car]
+        else:
+            result[car] = last_time-parking[car]
+            
+    result = sorted(result.items(), key=lambda x:x[0])
+   
+    answer = []
+    for car,time in result:
+        answer.append(b_f + math.ceil(max(0,time-b_t)/p_t)*p_f)
     
     return answer
