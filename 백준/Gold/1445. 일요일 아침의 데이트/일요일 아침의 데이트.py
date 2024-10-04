a0c73eeef00cacd12a944cb5f86af1bd6f90fcd6
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-04 23:52:19 +0900

    [Gold II] Title: 일요일 아침의 데이트, Time: 40 ms, Memory: 33220 KB -BaekjoonHub

--- 백준/Gold/1445. 일요일 아침의 데이트/일요일 아침의 데이트.py
+++ 백준/Gold/1445. 일요일 아침의 데이트/일요일 아침의 데이트.py
@@ -1,49 +1,56 @@
-# 쓰레기 정보 저장 배열 
-# 방문 최적 쓰레기 정보 저장 dictionary dic[(x,y)] = (a,b)
-# heapq.heappush(value, (a,b,x,y))
-#1:23
-import heapq
-
+from heapq import heappop, heappush
 N,M = map(int,input().split())
-graph =[ list(input()) for _ in range(N)]
-costs = {}
-dx =[0,0,1,-1,1,-1,1,-1]
-dy = [1,-1,0,0,1,-1,-1,1]
+graph = [list(input()) for _ in range(N)]
+values = [[(0,0)]*M for _ in range(N)]
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+visited = [[0]*M for _ in range(N)]
 
+def isOk(x,y):
+  if 0<=x<N and 0<=y<M:
+    return True
+  return False
 
 for i in range(N):
   for j in range(M):
-    if graph[i][j] == "F":
-      flower = (i,j)
-    elif graph[i][j] == "S":
-       start= (i,j)
-
-def check(x,y):
-  global N,M
-  if x == flower[0] and y == flower[1]:
-    return (0,0)
-  if graph[x][y] == "g":
-    return (1,0)
-  for i in range(4):
-    if 0<=x+dx[i]<N and 0<=y+dy[i]<M:
-      if graph[x+dx[i]][y+dy[i]] == "g":
-        return(0,1)
-  return (0,0)
-
+    if graph[i][j] == 'F':
+      start = (i,j)
+    elif graph[i][j] == 'S':
+      end = (i,j)
+    elif graph[i][j] == 'g':
+      values[i][j] = (1,0)
+    else:
+      for d in range(4):
+        if isOk(i+dx[d], j+dy[d]):
+          if graph[i+dx[d]][j+dy[d]] == 'g':
+            values[i][j] = (0,1)
+            break
 
 def dijkstra():
+  x,y = start
   q = []
-  heapq.heappush(q,(0,0,start[0],start[1]))
-  costs[start] = (0,0)
-  while(q):
-    a,b,x,y = heapq.heappop(q)
-    for d in range(4):
-      nx = x + dx[d]
-      ny = y + dy[d]
-      if 0<=nx<N and 0<=ny<M and (nx,ny) not in costs:
-        i,j = check(nx,ny)
-        costs[(nx,ny)] = (a+i, b+j)
-        heapq.heappush(q,(a+i,b+j,nx,ny))
+  heappush(q,(0, 0, x,y))
+  visited[x][y] = 1
 
-dijkstra()
-print(*costs[flower])
+  while(q):
+    g,f,cx,cy = heappop(q)
+
+    if (cx,cy) == end:
+      return g,f
+
+    for d in range(4):
+      nx = cx + dx[d]
+      ny = cy + dy[d]
+      if isOk(nx,ny) and not visited[nx][ny]:
+        visited[nx][ny] = 1
+        ng, nf = values[nx][ny]
+        heappush(q,(g+ng,f+nf,nx,ny))
+
+
+print(*dijkstra())
+
+
+
+
+#쓰레기통과 쓰레기 옆 통과
+
