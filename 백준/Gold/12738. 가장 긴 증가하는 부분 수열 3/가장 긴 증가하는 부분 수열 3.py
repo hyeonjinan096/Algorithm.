3a9f12c28a72ac50e04558f7fcaec8f26e3f8680
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-05 08:18:36 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 904 ms, Memory: 151248 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -1,21 +1,13 @@
-n = int(input())
-lis = list(map(int,input().split()))
-result = [-float('inf')]
+from bisect import bisect_left
+N = int(input())
+nums = list(map(int,input().split()))
+result = []
 
-def binary(idx):
-  if result[-1]< lis[idx]:
-    result.append(lis[idx])
-    return
-  l, r = 0,len(result)-1
-  while(l<=r):
-    mid = (l+r)//2
-    if result[mid] >=lis[idx]:
-      r = mid-1
-    else:
-      l = mid+1
-  result[l] = lis[idx]
+for num in nums:
+  idx = bisect_left(result,num)
+  if idx == len(result):
+    result.append(num)
+  else:
+    result[idx] = num
 
-for i in range(n):
-  binary(i)
-
-print(len(result)-1)
+print(len(result))
