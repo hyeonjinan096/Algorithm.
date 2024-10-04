44dbd6ab5836d6d02663f74b7b2d14745d452d90
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-04 22:51:09 +0900

    [level 3] Title: 주사위 고르기, Time: 5387.71 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/README.md
+++ 프로그래머스/3/258709. 주사위 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 주사위 고르기 - 258709 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 8379.94 ms
+메모리: 10.4 MB, 시간: 5387.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 12:24:42
+2024년 10월 04일 22:51:07
 
 ### 문제 설명
 
--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,39 +1,30 @@
 from itertools import combinations, product
 from bisect import bisect_left
-
-def solution(dices):
-    dic = {}
-    L = len(dices)
-    for A_index_combi in combinations(range(L), L//2):
-        B_index_combi = [i for i in range(L) if i not in A_index_combi]
-        print(A_index_combi)
-        A, B = [], []
-        for order_product in product(range(6), repeat=L//2):
-            # A_index_combi : 주사위 번호
-            # order_product: 주사위 면 번호
-            # zip(A_index_combi, order_product) : 주사위 마다 면 번호 i,j -> 주사위마다 면번호의 값을 sum으로 더함
-            A.append(sum(dices[i][j] for i, j in zip(A_index_combi, order_product)))
-            B.append(sum(dices[i][j] for i, j in zip(B_index_combi, order_product)))
+def solution(dice):
+    answer = []
+    #n개의 주사위 
+    n = len(dice)
+    Max = 0
+    
+    
+    for a_com in combinations(range(n),n//2):
+        b_com = [j for j in range(n) if j not in a_com]
+        A, B = [],[]
+        
+        for dice_com in product(range(6),repeat=n//2):
+            A.append(sum(dice[i][j] for i,j in zip(a_com, dice_com)))
+            B.append(sum(dice[i][j] for i,j in zip(b_com, dice_com)))
         B.sort()
-        
         wins = 0
-        for i in A:
-            l, r = 0,len(B)-1
-            while(l<=r):
-                mid = (l+r)//2
-                if B[mid] >= i:
-                    result = mid
-                    r = mid-1
-                else:
-                    l = mid+1
-            wins+=l
-        
-        
-            
+        for a in A:
+            wins+=bisect_left(B,a)
+        if Max < wins:
+            answer = a_com
+            Max = wins
 
-        # wins = sum(bisect_left(B, num) for num in A)
-        dic[wins] = list(A_index_combi)
+    return [i+1 for i in answer]
 
-    max_key = max(dic.keys())
-
-    return [x+1 for x in dic[max_key]]
+    #n개의 주사위를 2분류로 나눠야함 
+    #나눈 경우에 나올 수 있는 모든 합을 구해야함 
+    #각각 나온 모든 합을 이용해서 승패를 계산 해야함 (이분탐색)
+    #승이 높은 경우의 조합으로 출력해야함 
