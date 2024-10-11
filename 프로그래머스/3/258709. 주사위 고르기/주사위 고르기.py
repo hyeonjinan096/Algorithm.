247078c20013ec98fef29099eae427965a48d9a4
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-11 20:51:35 +0900

    [level 3] Title: 주사위 고르기, Time: 4881.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,30 +1,33 @@
 from itertools import combinations, product
 from bisect import bisect_left
+
 def solution(dice):
     answer = []
-    #n개의 주사위 
     n = len(dice)
     Max = 0
     
-    
     for a_com in combinations(range(n),n//2):
-        b_com = [j for j in range(n) if j not in a_com]
-        A, B = [],[]
+        b_com = [a for a in range(n) if a not in a_com]
         
-        for dice_com in product(range(6),repeat=n//2):
-            A.append(sum(dice[i][j] for i,j in zip(a_com, dice_com)))
-            B.append(sum(dice[i][j] for i,j in zip(b_com, dice_com)))
-        B.sort()
+        a_sum, b_sum = [],[]
+        for j in product(range(6),repeat = n//2):
+            a_sum.append(sum(dice[x][y] for x,y in zip(a_com, j)))
+            b_sum.append(sum(dice[x][y] for x,y in zip(b_com, j)))
+        b_sum.sort()
         wins = 0
-        for a in A:
-            wins+=bisect_left(B,a)
-        if Max < wins:
-            answer = a_com
+        for a in a_sum:
+            wins += bisect_left(b_sum, a)
+        if wins > Max:
             Max = wins
+            answer = a_com
+            
+            
+        
+    
+    
+    return [x+1 for x in answer]
 
-    return [i+1 for i in answer]
 
-    #n개의 주사위를 2분류로 나눠야함 
-    #나눈 경우에 나올 수 있는 모든 합을 구해야함 
-    #각각 나온 모든 합을 이용해서 승패를 계산 해야함 (이분탐색)
-    #승이 높은 경우의 조합으로 출력해야함 
+#주사위 종류 2개 조합 cobinations
+#주사위 2개에서 만들 수 있는 합  몇번째가 나오는 지 조합  permutations 
+# 두 주사위 합 모두 비교 -> 경우의 수 쌓임 가장 높은 경우 저장 
