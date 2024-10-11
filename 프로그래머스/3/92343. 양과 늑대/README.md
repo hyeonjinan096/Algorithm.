9ec1d5f69bb5c608f2a2326095367b6103510068
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-11 19:49:19 +0900

    [level 3] Title: 양과 늑대, Time: 15.56 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 22.77 ms
+메모리: 10 MB, 시간: 15.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 21:15:19
+2024년 10월 11일 19:49:17
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,24 +1,25 @@
 def solution(info, edges):
     answer = 0
     visited = [0]*len(info)
+    
 
     def dfs(sheep, wolf):
         nonlocal answer
-        
         if sheep <= wolf:
-            return
+            return 
         
-        answer = max(sheep, answer)
+        answer = max(answer, sheep)
+        
         for x,y in edges:
-            if visited[x] ==1 and visited[y] == 0:
+            if visited[x] == 1 and visited[y] == 0:
                 visited[y] = 1
                 if info[y] == 0:
-                    dfs(sheep+1, wolf)
+                    dfs(sheep+1,wolf)
                 else:
-                    dfs(sheep, wolf+1)
+                    dfs(sheep,wolf+1)
                 visited[y] = 0
-                
-    visited[0] = 1        
+    
+    visited[0] = 1
     dfs(1,0)
     
     return answer
