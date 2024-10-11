28a6d8654f7f8bce08c6ae5e51bdda564eaef0ec
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-11 20:09:07 +0900

    [level 3] Title: n + 1 카드게임, Time: 193.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,37 +1,39 @@
 def solution(coin, cards):
-    Round = 0#라운드 수 
     n = len(cards)
-    key = n+1 
-
-    mycard = cards[:n//3]
-    nextcard = []
+    key = n+1
     
-
-    def check (cards1, cards2):
+    def check(cards1, cards2):
         for card in cards1:
-            if key - card in cards2:
-                cards2.remove(key-card)
+            if key-card in cards2:
                 cards1.remove(card)
+                cards2.remove(key-card)
                 return True
         return False
-    
+
+    mycards = cards[:n//3]
+    nextcards = []
     idx = n//3
+    Round = 0
+    
     while(1):
         Round+=1
-        if idx == len(cards):
+        if idx+1 >= n:
             break
-        
         for _ in range(2):
-            nextcard.append(cards[idx])
+            nextcards.append(cards[idx])
             idx+=1
-
-        if check(mycard, mycard):
+    
+        if(check(mycards, mycards)):
             continue
-        elif coin>=1 and check(mycard, nextcard):
+        elif(coin>0 and check(mycards, nextcards)):
             coin-=1
-        elif coin>=2 and check(nextcard, nextcard):
+        elif(coin>1 and check(nextcards, nextcards)):
             coin-=2
         else:
             break
+        
+            
+            
+    
     
     return Round
