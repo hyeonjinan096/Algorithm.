db938387c330cc2b3754396fae91f4c417633fd2
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-11 21:27:15 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.58 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,25 +1,25 @@
 from collections import deque
-
 def solution(queue1, queue2):
-    #최소 작업 횟수, 없을 경우 -1
-    answer = 0
-    #합이 같아져야함
+    answer = -1
+    q1_sum = sum(queue1)
+    same = (q1_sum+sum(queue2))//2
     q1 = deque(queue1)
     q2 = deque(queue2)
-    sum_q1,sum_q2 = sum (q1) , sum(q2)
-    S = (sum_q1 + sum_q2)//2
     
+    cnt = 0
     for _ in range(300000):
-        if sum_q1 == S:
-            return answer
-        answer+=1
-        if sum_q1 < S:
-            tmp = q2.popleft()
-            q1.append(tmp)
-            sum_q1+=tmp
+        if q1_sum == same:
+            answer = cnt
+            break
+        if q1_sum < same:
+            new = q2.popleft()
+            q1.append(new)
+            q1_sum += new
         else:
-            tmp = q1.popleft()
-            q2.append(tmp)
-            sum_q1-=tmp
+            new = q1.popleft()
+            q2.append(new)
+            q1_sum -= new
+        cnt+=1
+            
     
-    return -1
+    return answer
