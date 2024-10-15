40a75d21aa119ae497134f7faf90ea1ceee8e252
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-16 01:21:56 +0900

    [Silver V] Title: CD, Time: 2040 ms, Memory: 143344 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -1,32 +1,18 @@
 import sys
-input=sys.stdin.readline
-
 
 while True:
-    N,M=map(int,input().split())
+   N, M = map(int,sys.stdin.readline().split())
+   if N == 0 and M == 0:
+      break
+   dic = {}
+   Cd_count = 0
+   for _ in range(N):
+      Cd = int(sys.stdin.readline())
+      dic[Cd] = 1
 
-    if N==0 and M==0:
-        exit(0)
+   for _ in range(M):
+      Cd = int(sys.stdin.readline())
+      if Cd in dic:
+         Cd_count += 1
 
-    N_list=[] ; M_list=[]
-
-    for i in range(N):
-        N_list.append(int(input()))
-
-    for i in range(M):
-        M_list.append(int(input()))
-
-    index_N=0 ; index_M=0 ; total=0
-
-    while index_N<N and index_M<M:
-
-        if N_list[index_N]==M_list[index_M]:
-            total+=1
-            index_N+=1 ; index_M+=1
-
-        elif N_list[index_N]>M_list[index_M]:
-            index_M+=1
-        else:
-            index_N+=1
-
-    print(total)   
+   print(Cd_count)
