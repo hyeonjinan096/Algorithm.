b8c9c5fce8f3d7670000f57c69e0c5b25bffd3c5
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-30 06:12:00 +0900

    [Gold V] Title: 1학년, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -1,19 +1,17 @@
 import sys
 
 n = int(input())
-nums = list(map(int, sys.stdin.readline().split()))
+nums = list(map(int,sys.stdin.readline().split()))
 
 dp = [[0]*21 for _ in range(n-1)]
 dp[0][nums[0]] = 1
 
-for i in range(1,n-1):
+for i in range(1, n-1):
   for j in range(21):
-    if dp[i-1][j] > 0:
+    if dp[i-1][j]>0:
       if 0<=j+nums[i]<=20:
-        dp[i][j+nums[i]] +=dp[i-1][j]
+        dp[i][j+nums[i]] += dp[i-1][j]
       if 0<=j-nums[i]<=20:
-        dp[i][j-nums[i]] +=dp[i-1][j]
-      
+        dp[i][j-nums[i]] += dp[i-1][j]
 
 print(dp[-1][nums[-1]])
-
