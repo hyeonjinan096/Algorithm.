7a6f0e58a5b9f40cd58512504a4ab72f1feac999
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-30 04:48:25 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,29 +1,36 @@
 import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
-T = int(input())
-dx=[0,0,-1,1]
-dy = [1,-1,0,0]
+T = int(sys.stdin.readline())
 
 for _ in range(T):
-  answer =0
-  M,N,K = map(int,input().split())
+  N, M, K = map(int,sys.stdin.readline().split())
   graph = [[0]*M for _ in range(N)]
-  for _ in range(K):
-    y,x = map(int,input().split())
-    graph[x][y] = 1
-  
-  def dfs(x,y):
-    graph[x][y] = 2
-    for i in range(4):
-      if 0<=x+dx[i]<N and 0<=y+dy[i]<M:
-        if graph[x+dx[i]][y+dy[i]] == 1:
-          dfs(x+dx[i], y+dy[i])
 
+  for _ in range(K):
+    x,y = map(int,sys.stdin.readline().split())
+    graph[x][y] = 1
+
+  dic = [(0,1),(0,-1),(1,0),(-1,0)]
+
+  def bfs(start):
+    q = deque([start])
+    while(q):
+      cx, cy = q.popleft()
+      for dx, dy in dic:
+        nx = cx+dx
+        ny = cy+dy
+        if 0<=nx<N and 0<=ny<M and graph[nx][ny] ==1:
+          graph[nx][ny] = 2
+          q.append([nx,ny])
+
+
+  answer = 0
   for i in range(N):
     for j in range(M):
-      if graph[i][j] ==1:
-        dfs(i,j)
+      if graph[i][j] == 1:
+        graph[i][j] = 2
+        bfs([i,j])
         answer +=1
+
   print(answer)
-      
