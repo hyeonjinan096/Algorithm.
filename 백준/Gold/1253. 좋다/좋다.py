f4915420c8b3dba0d4bf5ee0e930602c81021e93
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-10-30 05:46:28 +0900

    [Gold IV] Title: 좋다, Time: 1072 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,25 +1,24 @@
 import sys
-input = sys.stdin.readline
-
 n = int(input())
-arr = list(map(int,input().split()))
-arr.sort()
+nums = list(map(int,sys.stdin.readline().split()))
+answer = 0
+nums.sort()
 
-cnt = 0
 for i in range(n):
-  goal = arr[i]
-  tmp = arr[:i] + arr[i + 1:]
-  start = 0
-  end = len(tmp)-1
-  while start < end:
-    Sum = tmp[start] + tmp[end]
-    if Sum == goal:
-      cnt+=1
+  target = nums[i]
+  tmp_nums = nums[:i] + nums[i+1:]
+  
+  l, r = 0,n-2
+  while(l<r):
+    num = tmp_nums[l] + tmp_nums[r] 
+    if num == target:
+      answer+=1
       break
-    elif Sum > goal:
-      end-=1
-    elif Sum < goal:
-      start+=1
+    elif num>target:
+      r -=1
+    else:
+      l +=1
 
-print(cnt)
-       
+print(answer)
+
+
