4bd88ddb8b30c33306a19034fc3a14840a592e4e
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-01 17:30:56 +0900

    [Gold IV] Title: 좋다, Time: 216 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.js
+++ 백준/Gold/1253. 좋다/좋다.js
@@ -3,21 +3,23 @@ const input = fs.readFileSync('/dev/stdin','utf8').trim().split('\n')
 const N = Number(input.shift())
 const values = input[0].trim().split(' ').map(Number)
 let answer = 0
+
+values.sort((a,b)=>a-b)
+
 for (let [idx, value] of values.entries()){
     let lst = [...values]
     lst.splice(idx,1)
 
-    for (let i = 0;i<N-1;i++){
-        let flag = 0
-        for (let j = i+1;j<N-1;j++){
-            if (lst[i]+lst[j] === value) {
-                flag =1
-                break
-            }
-        }
-        if (flag) {
+    let start = 0
+    let end = lst.length-1
+    while(start<end){
+        if (lst[start] + lst[end] === value) {
             answer++
             break
+        } else if (lst[start] + lst[end] < value){
+            start++
+        } else {
+            end--
         }
     }
 }
