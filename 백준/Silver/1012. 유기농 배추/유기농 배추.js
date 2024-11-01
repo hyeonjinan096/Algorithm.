c5d654f4d8b9879fcca7fcbc483680b912ceef7e
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-01 16:48:22 +0900

    [Silver II] Title: 유기농 배추, Time: 172 ms, Memory: 12544 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin','utf8').trim().split('\n')
+
+const T = Number(input.shift().trim())
+
+const dir = [[1,0],[-1,0],[0,1],[0,-1]]
+
+for (let i = 0;i<T;i++){
+    let answer = 0
+    const [N,M,K] = input.shift().trim().split(' ').map(Number)
+    const graph = [...Array(N)].map(()=>Array(M).fill(0))
+    for (let j =0;j<K;j++){
+        const [x,y] = input.shift().trim().split(' ').map(Number)
+        graph[x][y] = 1
+    }
+
+
+    //bfs로 탐색하면서 연결되어있는 1인 곳만 방문하고 방문 처리 2
+    function bfs(x,y){
+        const q = [[x,y]]
+        graph[x][y] = 2
+        while(q.length){
+            const [cx,cy] = q.shift()
+            for (let [dx, dy] of dir){
+                let nx = cx+ dx
+                let ny = cy+ dy
+
+                if (nx>=0 && nx<N && ny>=0 && ny<M && graph[nx][ny] ===1){
+                    graph[nx][ny] = 2
+                    q.push([nx,ny])
+                }
+            }
+        }
+    }
+
+    //for문 돌면서 1인 공 방문하고 거기서 퍼지는 거 확인하기 위해 bfs 돌리기
+    for (let i=0;i<N;i++){
+        for (let j =0;j<M;j++){
+            if (graph[i][j] === 1){
+                bfs(i,j)
+                answer++
+            }
+        }
+    }
+
+    console.log(answer)
+}
