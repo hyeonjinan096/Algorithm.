367afe41dc308c193e3b16a560357df104513fb8
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-02 08:11:30 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 1272 ms, Memory: 325628 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.js
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.js
@@ -2,40 +2,41 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin','utf8').trim().split('\n')
 
 const N = Number(input.shift())
-const values = input[0].trim().split(' ').map(Number)
+const values = input.shift().trim().split(' ').map(Number)
 const result = []
-const idx_result = []
+const idx_result = Array(N).fill(0)
 
+//이중탐색 
 function binarySearch(arr,value){
     let start = 0
     let end = arr.length-1
     while(start<=end){
-        let mid = Math.floor((start + end)/2)
-        if (arr[mid] >= value){
+        let mid = Math.floor((start+end)/2)
+        if (arr[mid]>=value){
             end = mid-1
         } else {
             start = mid+1
         }
     }
-
     return start
 }
 
 
-for (let [i,value] of values.entries()){
-    let idx = binarySearch(result,value)
-    if (idx === result.length){
+//values 탐색
+for (let [idx,value] of values.entries()){
+    const index = binarySearch(result, value)
+    if (index === result.length){
         result.push(value)
-    }else {
-        result[idx] = value
+    } else {
+        result[index] = value
     }
-
-    idx_result[i] = idx
+    idx_result[idx] = index
 }
 
+const answer = []
 let order = Math.max(...idx_result)
-let answer = []
-for (let i=N-1;i>-1;i--){
+
+for (let i = N-1;i>-1;i--){
     if (idx_result[i] === order){
         answer.push(values[i])
         order--
