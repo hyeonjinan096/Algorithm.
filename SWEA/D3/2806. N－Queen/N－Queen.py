c8b0cfee448d663fd2249a71cc58f89524ef4fff
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-08 12:11:33 +0900

    [D3] Title: N-Queen, Time: 173 ms, Memory: 50,540 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,71 +1,25 @@
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
+T = int(input())
 
-# 표준 입력 예제
-'''
-a = int(input())                        정수형 변수 1개 입력 받는 예제
-b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제
-d = float(input())                      실수형 변수 1개 입력 받는 예제
-e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
-h = input()                             문자열 변수 1개 입력 받는 예제
-'''
-
-# 표준 출력 예제
-'''
-a, b = 6, 3
-c, d, e = 1.0, 2.5, 3.4
-f = "ABC"
-print(a)                                정수형 변수 1개 출력하는 예제
-print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
-print(c, d, e)                          실수형 변수 3개 출력하는 예제
-print(f)                                문자열 1개 출력하는 예제
-'''
-
-
-
-
-'''
-아래의 구문은 input.txt 를 read only 형식으로 연 후,
-앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-아래 구문을 사용하기 위해서는 import sys가 필요합니다.
-단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
-#import sys
-#sys.stdin = open("input.txt", "r")
-
-def dfs(y, x):
-    global answer
-    if x == n-1:
+def dfs(x):
+    global answer,N
+    if x == N:
         answer+=1
         return
-    for i in range(n):
-        cx = x+1
-        if i not in vy and cx-i not in vr and cx+i not in vl:
-            vy.append(i)
-            vl.append(cx+i)
-            vr.append(cx-i)
-            dfs(i, cx)
-            vy.pop()
-            vl.pop()
-            vr.pop()
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    n = int(input())
-    answer = 0
-    vy = []
-    vl = []
-    vr =[]
+    for y in range(N): #0행의 열을 돈다.
+        if y not in visited[0] and (y-x) not in visited[1] and (y+x) not in visited[2]:
+          visited[0].append(y)
+          visited[1].append(y-x) #차
+          visited[2].append(y+x) #합
+          dfs(x+1)
+          visited[0].pop()
+          visited[1].pop()
+          visited[2].pop()
+        
 
-    for i in range(n):
-        vy.append(i)
-        vl.append(i)
-        vr.append(-i)
-        dfs(i,0)
-        vy.pop()
-        vl.pop()
-        vr.pop()
+for test_case in range(1, T + 1):
+    N = int(input())
+    visited = [[],[],[]] #열, 오른쪽 대각선, 왼쪽 대각선
+    answer = 0
+    dfs(0)
+
     print(f'#{test_case} {answer}')
