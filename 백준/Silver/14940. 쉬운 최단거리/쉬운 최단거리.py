f521cc17edf823f82b1e08da2f9ada9397f43d12
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 01:52:06 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 408 ms, Memory: 39660 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,36 +1,33 @@
 import sys
 from collections import deque
-n, m = map(int,sys.stdin.readline().split(' '))
-graph = [list(map(int,sys.stdin.readline().split(' '))) for _ in range(n)]
-visited = [[-1]*m for _ in range(n)]
-start = [0,0]
+N,M = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+visited = [[-1]*M for _ in range(N)]
 
-dir = [(0,1),(0,-1),(1,0),(-1,0)]
-
-def bfs(start):
-  q = deque([start])
-  while(q):
-    cx,cy = q.popleft()
-    for [dx,dy] in dir:
-      nx = cx+dx
-      ny = cy+dy
-      if nx <0 or nx>=n or ny<0 or ny>=m:
-        continue
-      if graph[nx][ny] == 0:
-        continue
-      elif graph[nx][ny] == 1 and (visited[nx][ny] > visited[cx][cy]+1 or visited[nx][ny]==-1):
-        visited[nx][ny] = visited[cx][cy]+1
-        q.append([nx,ny])
-
-
-for i in range(n):
-  for j in range(m):
+for i in range(N):
+  for j in range(M):
     if graph[i][j] == 2:
+      S = [i,j]
       visited[i][j] = 0
-      start = [i,j]
-    elif graph[i][j] == 0:
+    if graph[i][j] == 0:
       visited[i][j] = 0
-      
-bfs(start)
-for data in visited:
-  print(*data)
+
+dir = [(1,0),(-1,0),(0,1),(0,-1)]
+def bfs(S):
+  q = deque([[S[0],S[1]]])
+  while(q):
+    x,y = q.popleft()
+    for dx,dy in dir:
+      nx = x+dx
+      ny = y+dy
+      if 0<=nx<N and 0<=ny<M:
+        if graph[nx][ny] == 0:
+          continue
+        if visited[nx][ny]==-1 or visited[nx][ny] > visited[x][y]+1:
+          visited[nx][ny] = visited[x][y]+1
+          q.append([nx,ny])
+    
+bfs(S)
+
+for v in visited:
+  print(*v)
