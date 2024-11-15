eeef4c2339886bb0e1f682aa7f33dd335aecdb5a
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-15 23:43:06 +0900

    [Gold V] Title: 1학년, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -1,17 +1,16 @@
-#3:31
-import sys
-
-N = int(sys.stdin.readline())
-values = list(map(int,sys.stdin.readline().split()))
+N = int(input())
+values = list(map(int,input().split()))
+target = values.pop()
 dp = [[0]*21 for _ in range(N-1)]
 dp[0][values[0]] = 1
 
 for i in range(1,N-1):
-  for j in range(21):
-    if dp[i-1][j] > 0:
-      if 0<=j + values[i]<=20:
-        dp[i][j+values[i]]+=dp[i-1][j]
-      if 0<=j - values[i]<=20:
-        dp[i][j-values[i]]+=dp[i-1][j]
+   for j in range(21):
+      if dp[i-1][j] == 0:
+         continue
+      if 0<=j+values[i]<=20:
+         dp[i][j+values[i]] += dp[i-1][j]
+      if 0<=j-values[i]<=20:
+         dp[i][j-values[i]] += dp[i-1][j]
 
-print(dp[-1][values[-1]])
+print(dp[-1][target])
