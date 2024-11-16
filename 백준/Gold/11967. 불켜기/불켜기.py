4ede3a9d159089e82f457e9b8faac5065462f2d0
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 13:28:36 +0900

    [Gold II] Title: 불켜기, Time: 100 ms, Memory: 34540 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.py
+++ 백준/Gold/11967. 불켜기/불켜기.py
@@ -1,50 +1,55 @@
-from sys import stdin
-input = stdin.readline
-from collections import deque, defaultdict
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
 
-dr = (-1, 1, 0, 0)
-dc = (0, 0, -1, 1)
+N,M = map(int,sys.stdin.readline().split())
+dic = {}
+rooms = [[0]*N for _ in range(N)]
+rooms[0][0] = 1
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+q = deque([])
+answer = 1
 
-def bfs():
-    result = 1      # 불 켤 수 있는 방의 갯수
-    visited = [[0] * N for _ in range(N)]   # 방문 표시
-    visited[0][0] = 1
-    lights = [[0] * N for _ in range(N)]    # 불 켠 방 표시
-    lights[0][0] = 1
-    Q = deque([(0, 0)])
-    while Q:
-        r, c = Q.popleft()
-        for tr, tc in turnInfo[(r, c)]:   # 불 켤 수 있는 곳(딕셔너리 참조)
-            if not lights[tr][tc]:
-                lights[tr][tc] = 1      # 새로 불 켜기
-                result += 1
-                for d in range(4):      # 4방향 연결된 곳
-                    nr = tr + dr[d]
-                    nc = tc + dc[d]
-                    if not (0 <= nr < N and 0 <= nc < N):
-                        continue
-                    if visited[nr][nc]:     # 방문한 적 있으면(새로 연결되어 또 불을 켤 곳이 있을 수 있으므로)
-                        Q.append((nr, nc))  # 큐에 담기
-        
-        # 현 위치를 기준으로
-        for d in range(4):      # 4방향 연결된 곳
-            nr = r + dr[d]
-            nc = c + dc[d]
-            if not (0 <= nr < N and 0 <= nc < N):
-                continue
-            # 첫 방문인데 이미 불 켜진 곳이면
-            if not visited[nr][nc] and lights[nr][nc]:
-                Q.append((nr, nc))      # 재검사를 위해 큐에 담기
-                visited[nr][nc] = 1     # 방문 표시
-                
-    return result
+for i in range(M):
+  x,y,a,b = map(int,sys.stdin.readline().split())
+  if (x-1,y-1) not in dic:
+    dic[(x-1,y-1)] = []
+  dic[(x-1,y-1)].append((a-1,b-1))
 
+def visited_switch(x,y):
+  global rooms,q,answer
 
-# main
-N, M = map(int, input().split())
-turnInfo = defaultdict(list)	# 각 위치에서 불 켤 수 있는 위치 정보 저장
-for _ in range(M):
-    sr, sc, tr, tc = map(int, input().split())
-    turnInfo[(sr-1, sc-1)].append((tr-1, tc-1))
+  if (x,y) not in dic:
+    return 
+  for (i,j) in dic[(x,y)]:
+    if rooms[i][j] == 0:
+      rooms[i][j] = 1 
+      answer+=1
+      for k in range(4):
+        ni = i+dx[k]
+        nj = j+dy[k]
+        if 0<=ni<N and 0<=nj<N and rooms[ni][nj] == 2:
+          rooms[i][j] = 2
+          q.append((i,j))
+          break 
 
-print(bfs())
+def bfs(x,y):
+  global q
+  rooms[x][y] = 2
+  q.append((x,y))
+  while(q):
+    cx,cy = q.popleft()
+    visited_switch(cx,cy)
+    for i in range(4):
+      nx = cx+dx[i]
+      ny = cy + dy[i]
+      if 0<=nx<N and 0<=ny<N and rooms[nx][ny] == 1:
+        rooms[nx][ny] = 2
+        q.append((nx,ny))
+
+bfs(0,0)
+
+print(answer)
+#1-> 불켜진 방 + 방문할 수 있는 방
+#2-> 방문한 방
