82a8fda2baec90b036b72f5dfb1eac6d119f2b5b
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 21:08:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,36 @@
-import sys
+#단지수
+#단지에 속하는 집의 수
 from collections import deque
-N = int(sys.stdin.readline().strip())
 
-graph =[]
-for _ in range(N):
-    graph.append(list(map(int,sys.stdin.readline().strip())))
-visited =[[False]*N for _ in range(N)]
+N = int(input())
+graph = [list(input()) for _ in range(N)]
 
-def bsq(x,y,c):
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
-    visited[x][y] = True
-    q = deque()
-    q.append((x,y))
-    while(q):
-        cx,cy = q.popleft()
-        for i in range(4):
-            nx = cx +dx[i]
-            ny = cy +dy[i]
-            if(nx>=0 and nx<N and ny>=0 and ny<N):
-                if(graph[nx][ny]==1 and not visited[nx][ny]):
-                    visited[nx][ny]=True
-                    q.append((nx,ny))
-                    c+=1
-    return c
+dir = [[1,0],[-1,0],[0,1],[0,-1]]
+def bfs(x,y):
+  q = deque([[x,y]])
+  cnt = 1
+  while(q):
+    cx,cy = q.popleft()
+    for dx,dy in dir:
+      nx = cx+dx 
+      ny = cy+dy
+      if 0<=nx<N and 0<=ny<N and graph[nx][ny] == '1':
+        graph[nx][ny] = '2'
+        cnt+=1
+        q.append([nx,ny])
+  return cnt
 
-count =0
-arr=[]
+answer_cnt = 0
+answer = []
 
 for i in range(N):
-    for j in range(N):
-        if(graph[i][j]==1 and not visited[i][j]):
-            arr.append(bsq(i,j,1))
-            count+=1
+  for j in range(N):
+    if graph[i][j] == '1':
+      answer_cnt+=1
+      graph[i][j] = '2'
+      answer.append(bfs(i,j))
 
-
-arr.sort()
-print(count)
-for i in arr:
-    print(i)
-
-
+answer.sort()
+print(answer_cnt)
+for a in answer:
+  print(a)
