7110304933aafdb40f93c540dd141b188f832bad
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 12:46:47 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 88 ms, Memory: 35680 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,25 +1,27 @@
 from collections import deque
-N, K = map(int, input().split())
+N,M = map(int,input().split())
 Max = 100001
-values = [float("inf")]*Max
+dp = [float('inf')]*Max
 
-def bfs(start, end):
-  q = deque([start])
-  values[start] = 0
+def bfs():
+  global N,M
+  q = deque([N])
+  dp[N] = 0
 
   while(q):
-    e = q.popleft()
-    time = values[e]
+    cx = q.popleft()
+    if cx == M:
+      print(dp[M])
+      return
+    for nx in [cx-1,cx+1]:
+      if 0<=nx<Max and dp[nx]>dp[cx]+1:
+        dp[nx] = dp[cx]+1
+        q.append(nx)
+    if 0<=cx*2<Max and dp[cx*2]>dp[cx]:
+      dp[cx*2] = dp[cx]
+      q.append(cx*2)
 
-    if e == end:
-      return time
-    
-    for idx in [e-1,e+1]:
-      if 0<=idx<Max and values[idx]>time+1:
-        values[idx] = time+1
-        q.append(idx)
-    if 0<=e*2<Max and values[e*2]>time:
-      values[e*2] = time
-      q.append(e*2)
-  
-print(bfs(N,K))
+bfs()
+
+
+
