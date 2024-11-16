64fbf2fe42b58aef067264e344a5a63e93d247f9
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 13:41:33 +0900

    [Gold V] Title: 1학년, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -1,16 +1,18 @@
 N = int(input())
 values = list(map(int,input().split()))
-target = values.pop()
+
 dp = [[0]*21 for _ in range(N-1)]
 dp[0][values[0]] = 1
 
+#vlues 돌면서 만들 수 있는 거 check
 for i in range(1,N-1):
-   for j in range(21):
-      if dp[i-1][j] == 0:
-         continue
-      if 0<=j+values[i]<=20:
-         dp[i][j+values[i]] += dp[i-1][j]
-      if 0<=j-values[i]<=20:
-         dp[i][j-values[i]] += dp[i-1][j]
+  for j in range(21):
+    if dp[i-1][j] > 0:
+      if 0<=j+values[i]<21:
+        dp[i][j+values[i]]+=dp[i-1][j]
+      if 0<=j-values[i]<21:
+        dp[i][j-values[i]]+=dp[i-1][j]
 
-print(dp[-1][target])
+print(dp[-1][values[-1]])
+
+
