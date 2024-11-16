e56be630f84179fee8c0e56ab3a95dc9f739a649
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-11-16 23:45:08 +0900

    [level 3] Title: 정수 삼각형, Time: 71.22 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
 
 ### 성능 요약
 
-메모리: 14.7 MB, 시간: 44.37 ms
+메모리: 14.6 MB, 시간: 71.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 23:46:41
+2024년 11월 16일 23:45:06
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,12 @@
 def solution(triangle):
     answer = 0
-    n = len(triangle)
-    
-    for i in range(1, n):
-        for j in range(len(triangle[i])):
-            if j == 0:
-                triangle[i][j] += triangle[i-1][j]
-            elif j == i:
-                triangle[i][j] += triangle[i-1][j-1]
-            else:
-                triangle[i][j] += max(triangle[i-1][j],triangle[i-1][j-1])
-
+    for i in range(1,len(triangle)):
+        for j in range(0,len(triangle[i])):
+            arr = []
+            if 0<=i-1 and 0<=j-1:
+                arr.append(triangle[i-1][j-1])
+            if 0<=i-1 and 0<=j<=i-1:
+                arr.append(triangle[i-1][j])
+            triangle[i][j] += max(arr)
     return max(triangle[-1])
+
