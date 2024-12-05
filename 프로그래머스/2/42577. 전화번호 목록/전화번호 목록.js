fd193a8c4f7d1a100b2a7e472c288bb9ddebb659
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-12-05 11:35:28 +0900

    [level 2] Title: 전화번호 목록, Time: 1703.30 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,5 +1,11 @@
 // 1,000,000 -> O(n)
 function solution(phone_book) {
-    let answer = phone_book.sort().some((value,index,self)=>self[index+1]?.indexOf(value) === 0)
-    return !answer;
+    phone_book.sort()
+    for (let i=0;i<phone_book.length-1;i++){
+        let reg = RegExp('^'+phone_book[i])
+        if (reg.test(phone_book[i+1])){
+           return false
+       }  
+    }
+    return true;
 }
