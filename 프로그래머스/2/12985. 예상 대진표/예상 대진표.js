9f51b5512ed7ded2bd104d9661d4347418dba250
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-12-05 12:20:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,17 +1,13 @@
-//11:39
 function solution(n,a,b)
 {
     var answer = 0;
-    while(1){
+    while(a!==b){
         answer++
-        a = Math.max(Math.ceil(a/2),1)
-        b = Math.max(Math.ceil(b/2),1)
-        if(a === b){break}
+        a = Math.ceil(a/2)
+        b = Math.ceil(b/2)
     }
     
     return answer;
 }
-
-solution(10,1,7)
 
 
