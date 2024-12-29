191361f09406f64abb3899207c92d564e678883a
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-12-29 14:06:51 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
@@ -2,48 +2,24 @@ import sys
 from collections import deque
 
 N,M = map(int,input().split())
-Map = [list(map(int,input().split())) for _ in range(N)]
-visited = [[float('INF')]*M for _ in range(N)]
+fuel_list = [list(map(int,input().split())) for _ in range(N)]
+min_fuel_list = [[[float('inf')]*3 for _ in range(M)] for _ in range(N)]
+
+for i in range(M):
+  min_fuel_list[0][i] = [fuel_list[0][i]]*3
+
+for i in range(1,N):
+  for j in range(M):
+    for k in range(3):
+      if (j == 0 and k == 2) or (j == M-1 and k == 0):
+        continue
+      for l in range(3):
+        if k == l:
+          continue
+        min_fuel_list[i][j][k] = min(min_fuel_list[i][j][k], min_fuel_list[i-1][j-k+1][l] + fuel_list[i][j])
 
 
 answer = float('inf')
-sum = 0
-dy = [-1,0,1]
-
-def dfs(x,y,pd):
-  global sum, answer
-  if x == N-1: 
-    answer = min(answer, sum)
-  for d in dy : 
-    if d!=pd and 0<=y+d<M and 0<=x+1<N:
-      sum += Map[x+1][y+d]
-      dfs(x+1,y+d,d)
-      sum -= Map[x+1][y+d]
-
 for i in range(M):
-  sum+=Map[0][i]
-  dfs(0,i,3)
-  sum-=Map[0][i]
-
-
+  answer = min(min(min_fuel_list[N-1][i]),answer)
 print(answer)
-
-
-# def bfs():
-#   q = deque()
-#   for i in range(M):
-#     q.append([0,i])
-#     visited[0][i] = Map[0][i]
-#   while(q):
-#     x,y=q.popleft()
-#     for d in dy:
-#       if 0<=y+d<M and 0<=x+1<N:
-#         w = visited[x][y] + Map[x+1][y+d]
-#         if w < visited[x+1][y+d] :
-#           visited[x+1][y+d] = w
-#           q.append([x+1,y+d])
-    
-  
-# bfs()
-  
-# print(visited)
