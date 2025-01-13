cc9a03797756cc8c8bf8234ab1d8f44093b9f403
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-01-13 12:22:31 +0900

    [Gold V] Title: 문자열 게임 2, Time: 404 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -1,28 +1,25 @@
 from collections import deque
+
 T = int(input())
 for _ in range(T):
     W = list(input())
     K = int(input())
-    dic = {}
-    Min = float("inf")
+    Min = float('inf')
     Max = 0
+    char_dict = {}
 
     for idx, val in enumerate(W):
-        if val not in dic:
-            dic[val] = deque()
-        dic[val].append(idx) #idx, level
+        if val not in char_dict:
+            char_dict[val] = deque([])
+        char_dict[val].append(idx)
 
-        if len(dic[val]) == K:
-            L = idx - dic[val][0] +1
-            Max = max(L,Max)
-            Min = min(L,Min)
-            dic[val].popleft()
+        if len(char_dict[val]) == K:
+            L = idx - char_dict[val][0] + 1
+            Min = min(L, Min)
+            Max = max(L, Max)
+            char_dict[val].popleft()
 
-
-    if (Min == float('inf') and Max == 0):
+    if Min == float('inf') and Max == 0:
         print(-1)
     else:
         print(Min, Max)
-
-
-
