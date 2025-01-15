cde2e9fc366ea9658b1a7007cd4f93f8bf088327
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-01-15 15:11:01 +0900

    [Gold IV] Title: 여행 가자, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,33 +1,34 @@
-# 유니온 파인드 함수 정의
-def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])  # 경로 압축
+def union(x,y):
+    x = find_parent(x)
+    y = find_parent(y)
+
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+def find_parent(x):
+    if x != parent[x]:
+        parent[x] = find_parent(parent[x])
     return parent[x]
 
-def union(parent, x, y):
-    root_x = find(parent, x)
-    root_y = find(parent, y)
-    if root_x != root_y:
-        parent[root_y] = root_x
-
-# 입력 처리
 N = int(input())
 M = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
-travel_route = list(map(int, input().split()))
-
-# 유니온 파인드 초기화
 parent = [i for i in range(N)]
-
-# 그래프를 기반으로 연결된 도시들을 병합
 for i in range(N):
+    lst = list(map(int,input().split()))
     for j in range(N):
-        if graph[i][j] == 1:
-            union(parent, i, j)
+        if lst[j] == 1:
+            union(i,j)
 
-# 여행 경로의 모든 도시가 같은 집합인지 확인
-root = find(parent, travel_route[0] - 1)  # 첫 번째 도시의 루트
-if all(find(parent, city - 1) == root for city in travel_route):
-    print("YES")
+route = [x-1 for x in map(int,input().split())]
+
+start = parent[route[0]]
+for i in range(M):
+    if parent[route[i]] != start:
+        print("NO")
+        break
 else:
-    print("NO")
+    print("YES")
+
+
