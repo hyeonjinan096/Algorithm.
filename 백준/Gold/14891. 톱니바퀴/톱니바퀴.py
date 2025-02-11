6b65de0f841a63e188ccd1135b851d43142b608a
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-02-11 13:24:47 +0900

    [Gold V] Title: 톱니바퀴, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -0,0 +1,39 @@
+lst = [list(map(int,list(input()))) for _ in range(4)]
+N = int(input())
+turns = [list(map(int,input().split())) for _ in range(N)]
+
+def turn(i,d):
+    if d == 1:
+        new_lst = [lst[i][7]] +  lst[i][:7]
+    else:
+        new_lst = lst[i][1:] + [lst[i][0]]
+
+    if d == 1:
+        rev_d = -1
+    else:
+        rev_d = 1
+
+    if i-1 >= 0 and not visited[i-1] and lst[i][6] != lst[i-1][2]:
+        visited[i-1] = 1
+        turn(i-1, rev_d)
+
+    if i+1 < 4 and not visited[i+1] and lst[i][2] != lst[i+1][6]:
+        visited[i+1] = 1
+        turn(i+1, rev_d)
+
+    lst[i] = new_lst
+
+
+
+
+for n,d in turns:
+    visited = [0]*4
+    visited[n-1] = 1
+    turn(n-1,d)
+
+score = [1,2,4,8]
+answer = 0
+for i in range(4):
+    answer += lst[i][0] * score[i]
+
+print(answer)
