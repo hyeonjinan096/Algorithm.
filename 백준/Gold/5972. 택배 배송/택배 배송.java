84629e9df8a830c153849d1e08d074942e6a4839
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-06-01 00:43:19 +0900

    [Gold V] Title: 택배 배송, Time: 388 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -7,51 +7,52 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         List<List<int[]>> graph = new ArrayList<>();
-        for(int i=0;i<=N;i++){
+        for(int i=0;i<N+1;i++){
             graph.add(new ArrayList<>());
         }
         
         for (int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(new int[]{b,c});
-            graph.get(b).add(new int[]{a,c});
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            graph.get(A).add(new int[]{B,C});
+            graph.get(B).add(new int[]{A,C});
         }
 
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x->x[1]));
         pq.offer(new int[]{1,0});
 
-        while(!pq.isEmpty()) {
+        int[] dp = new int[N+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
-            int now = cur[0];
+
+            int x = cur[0];
             int cost = cur[1];
 
-            if (now == N) break;
-            if (cost > dist[now]) continue;
+            if (x == N) break;
+            if (cost > dp[x]) continue;
 
-            for(int[] next : graph.get(now)) {
-                int to = next[0];
-                int ncost = cost + next[1];
-                if(ncost < dist[to]) {
-                    dist[to] = ncost;
-                    pq.offer(new int[]{to,ncost});
-                }
+            for(int[] next : graph.get(x)){
+                int next_x = next[0];
+                int next_cost = cost + next[1];
+                
+                if (dp[next_x] <= next_cost) continue;
+
+                dp[next_x] = next_cost;
+                pq.offer(new int[]{next_x,next_cost});
             }
         }
 
-        System.out.println(dist[N]);    
-            
+        System.out.println(dp[N]);
+
+        
+        
     }
 }
