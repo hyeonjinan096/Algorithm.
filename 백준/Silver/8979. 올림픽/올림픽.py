2c1cebca1695df416fc80a75698e45b35d23f33f
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-06-01 22:22:24 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -11,8 +11,9 @@ l = 1
 obj = {arr[0][0]:l}
 
 for i in range(1,N):
-    if (arr[i-1][1] != arr[i][1] or arr[i-1][2] != arr[i][2] or arr[i-1][3] != arr[i][3]):
-        l+=1
-    obj[arr[i][0]] = l
+    if i == 0 or (arr[i-1][1] != arr[i][1] or arr[i-1][2] != arr[i][2] or arr[i-1][3] != arr[i][3]):
+        obj[arr[i][0]] = i+1
+    else:
+        obj[arr[i][0]] = obj[arr[i-1][0]]
 
 print(obj[M])
