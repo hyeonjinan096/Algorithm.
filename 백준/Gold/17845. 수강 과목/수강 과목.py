3546a7453bf70bf3d076e5ab31466b84ffbf8cf4
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-06-02 14:19:36 +0900

    [Gold V] Title: 수강 과목, Time: 1864 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.py
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -1,19 +1,12 @@
-N, K = map(int,input().split())
+N,K = map(int,input().split())
+lst = [list(map(int,input().split())) for _ in range(K)]
 
-subjects = []
 
-for _ in range(K):
-    I,T = map(int,input().split())
-    subjects.append([I,T])
-
-#최대 중요도 공부 시간별 - 중요도 
-dp = [0] * (N+1)
-
-for  i in range(K):
-    important = subjects[i][0]
-    time = subjects[i][1]
-
-    for j in range(N, time-1, -1):
-        dp[j] = max(dp[j], dp[j - time] + important)
+dp = [0] * 10001
+for i in range(K):
+    important = lst[i][0]
+    time = lst[i][1]
+    for j in range(N,time-1,-1):
+        dp[j] = max(dp[j],dp[j -time] + important)
 
 print(dp[N])
