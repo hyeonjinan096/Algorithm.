46dafdd18eb0ca1b39d0e258dcf2273f269e0b28
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-07-29 08:32:40 +0900

    [level 3] Title: 징검다리 건너기, Time: 268.01 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=python) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 15.7 MB, 시간: 248.49 ms
+메모리: 15.6 MB, 시간: 268.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 23:55:13
+2025년 07월 29일 08:32:37
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -2,22 +2,22 @@ def solution(stones, k):
     answer = 0
     start = 1
     end = 200000000
-    
+
     while(start<=end):
         mid = (start + end)//2
         flag = 0
         cnt = 0
         for stone in stones:
-            if stone-mid <=0:
-                cnt +=1
+            if stone-mid <= 0:
+                cnt += 1
                 if cnt >= k:
                     flag = 1
                     break
             else:
                 cnt = 0
         if flag:
-            end = mid-1
+            end = mid - 1
         else:
-            start = mid+1
+            start = mid + 1
     
     return start
